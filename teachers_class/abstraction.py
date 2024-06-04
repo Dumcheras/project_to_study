fbd2eafb0f23392f1c89985de275cf25dd1dc111
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-04 14:48:09 +0300

    добавлен блок инициализации\\add initialization block

---
+++ teachers_class/abstraction.py
@@ -0,0 +1,6 @@
+class Teachers:
+    # блок инициализации\\initialization block
+    def __init__(self, name_surname, job_title, discipline):
+        self._name_surname = name_surname
+        self._job_title = job_title
+        self._discipline = discipline
